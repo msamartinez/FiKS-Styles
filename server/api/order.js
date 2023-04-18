47d60c274c3aaa8afb9ab52e9bf6054ed82d5549
fionaxu2023 <xuliefan@hotmail.com>
2023-04-18 06:00:34 -0400

    change in seed

---
+++ server/api/order.js
@@ -0,0 +1,25 @@
+const router = require('express').Router()
+const {OrderItem, Product} = require('../db/models')
+
+router.get('/cart', async (req, res, next) => {
+    try {
+      const order = await OrderItem.findOne({
+        where: {
+          userId: req.user.id,
+          status: 'pending'
+        },
+        include: [{model: Product}]
+      })
+  
+      res.json(order)
+    } catch (error) {
+      next(error)
+    }
+  })
+
+
+
+
+
+
+module.exports = router
