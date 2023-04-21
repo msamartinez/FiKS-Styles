1a7ec9f96701b715be0c1701ac3ecaa33d99b7e2
reginar-kim <kimregina.r@gmail.com>
2023-04-21 12:35:12 -0400

    Created confirmation component

--- client/components/checkout/confirmation.js
+++ client/components/checkout/confirmation.js
@@ -1,2 +1,14 @@
-//! changed the file name to Confirmation.js
-//* lets capitalize first letters for component files to match other files
+
+import React from "react";
+
+function Confirmation() {
+  return (
+    <div>
+      <h2>Thank you for your purchase!</h2>
+      <h1>ORDER NUMBER</h1> //! this will be the order number from the database or stripe or whatever 
+      <p>Your order has been confirmed and will be shipped soon.</p>
+    </div>
+  );
+}
+
+export default Confirmation;
