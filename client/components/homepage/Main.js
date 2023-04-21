2d5021deec5f78dfdd4783c9d004f9d0d50b5e51
Mary Stephanie Martinez <msamartinez1@gmail.com>
2023-04-20 22:21:37 -0400

    made changes to product component

--- client/components/homepage/Main.js
+++ client/components/homepage/Main.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Products from './Products';
+
+
 const Main=()=>{
  return (
     <div>
         main
+        <br/>
+        <Routes>
+            <Route path='/products' element={<Products />} />
+            <Route path='/products/:id' element={<Products />} />
+        </Routes>
+
     </div>
  )
 }
 
-export default Main 
+export default Main;
--- client/components/homepage/Products.js
+++ client/components/homepage/Products.js
@@ -1,19 +1,31 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-// insert slices
+import { fetchProducts, fetchSingleProduct } from '../../store/productSlice';
 
 const Products = () => {
-    const tops = useSelector();
-    const bottoms = useSelector();
-    const dispatch = useDispatch;
+    const allProducts = useSelector(fetchProducts);
+    const product = useSelector(fetchSingleProduct);
+    const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch();
-    }, [dispatch]);
+        dispatch(fetchProducts());
+    }, [dispatch, id]);
 
     return (
         <div>
             <h2>All Products</h2>
+            <br/>
+            {allProducts && allProducts.length
+            ? allProducts.map((product) => (
+                <div key ={product.id}>
+                    <Link 
+                    to={`/products/${product.id}`}key={`All Products: ${product.id}`}>
+                        <img src={product.imageUrl}/>
+                        <h2>{product.name}</h2>
+                    </Link>
+                </div>
+            ))
+            :null}
         </div>
     );
 };
