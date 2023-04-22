26e8f3f165d1e1f99dc628a0acfaf635b1f19501
reginar-kim <kimregina.r@gmail.com>
2023-04-21 23:18:57 -0400

    Updated AboutContact but forgot to create a different branch

--- server/api/index.js
+++ server/api/index.js
@@ -7,6 +7,7 @@ router.use("/orderitem", require("./orderItem"))
 router.use("/orderdetail" , require("./orderDetail"))
 // router.use("/checkout",require("./stripe"))
 
+
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
