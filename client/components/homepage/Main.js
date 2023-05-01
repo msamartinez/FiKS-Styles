b5f83bd6dc7dabd7888a15f1175048e547b3d35f
Mary Stephanie Martinez <msamartinez1@gmail.com>
2023-04-30 22:20:43 -0400

    pagination complete

--- client/components/homepage/Main.js
+++ client/components/homepage/Main.js
@@ -1,28 +1,13 @@
-import React, { useState } from 'react';
-import Pagination from '@mui/material/Pagination';
-import Stack from '@mui/material/Stack';
+import React from 'react';
+
 
 import Products from './Products';
 
 const Main = () => {
-   const [currentPage, setCurrentPage] = useState(1);
-   
-   const handlePageChange = (event, value) => {
-      setCurrentPage(value);
-   };
 
  return (
     <div>
-       <Products currentPage={currentPage} />
-       <Stack spacing ={2}>
-         <Pagination
-         count={10}
-         page={currentPage}
-         onChange={handlePageChange}
-         variant='outlined'
-         shape='rounded'
-         />
-       </Stack>
+      <Products/>
     </div>
    );
  };
--- client/components/homepage/Products.js
+++ client/components/homepage/Products.js
@@ -1,79 +1,99 @@
-import React, { useEffect ,useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import {fetchProducts } from "../../store/productSlice"
+import { fetchProducts } from "../../store/productSlice";
 import Tabs from "@mui/material/Tabs";
 import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import { Typography } from "@mui/material";
 import Product from "./SingleProduct";
+import Pagination from "@mui/material/Pagination";
+import Stack from "@mui/material/Stack";
 
-const Products = ({currentPage}) => {
-    const products = useSelector((state)=> state.products.allProducts);
-    const dispatch = useDispatch();
-    const [value, setValue] = useState("all");
-    const cartitems= useSelector((state) => state.cart.items);
-    const productsPerPage = 10;
-    const startIndex = (currentPage - 1) * productsPerPage;  
-    const endIndex = startIndex + productsPerPage;
+const Products = () => {
+  const products = useSelector((state) => state.products.allProducts);
+  const dispatch = useDispatch();
+  const [value, setValue] = useState("all");
+  const [currentPage, setCurrentPage] = useState(1);
+  const cartitems = useSelector((state) => state.cart.items);
+  const productsPerPage = 10;
+  const startIndex = (currentPage - 1) * productsPerPage;
+  const endIndex = startIndex + productsPerPage;
 
-    const handleChange = (event, newValue) => {
-        setValue(newValue);
-      };
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+    setCurrentPage(1);
+  };
 
-    useEffect(() => {
-        dispatch(fetchProducts());
-    }, [dispatch]);
-   
-    const Top = products.filter((product) => product.category === "Top");
-    const Bottom = products.filter((product) => product.category === "Bottom");
-    const Dress = products.filter((product) => product.category === "Dress");
+  useEffect(() => {
+    dispatch(fetchProducts());
+  }, [dispatch]);
 
-    const filteredProducts =
-      value === "all"
-        ? products.slice(startIndex, endIndex)
-        : value === "TOP"
-          ? Top.slice(startIndex, endIndex)
-          : value === "BOTTOM"
-            ? Bottom.slice(startIndex, endIndex)
-            : Dress.slice(startIndex, endIndex);
+  const Top = products.filter((product) => product.category === "Top");
+  const Bottom = products.filter((product) => product.category === "Bottom");
+  const Dress = products.filter((product) => product.category === "Dress");
 
-    return (
-        <Box width="80%" margin="80px auto">
-          <Typography variant="h3" textAlign="center">
-            Our Featured <b>Products</b>
-          </Typography>
-          <Tabs
-            textColor="primary"
-            indicatorColor="primary"
-            value={value}
-            onChange={handleChange}
-            centered
-            sx={{
-              m: "25px",
-              "& .MuiTabs-flexContainer": {
-                flexWrap: "wrap",
-              },
-            }}
-          >
-            <Tab label="ALL" value="all" />
-            <Tab label="TOP" value="TOP" />
-            <Tab label="BOTTOM" value="BOTTOM" />
-            <Tab label="DRESS" value="DRESS" />
-          </Tabs>
-          <Box
-            margin="0 auto"
-            display="grid"
-            gridTemplateColumns="repeat(auto-fill, 300px)"
-            justifyContent="space-around"
-            rowGap="20px"
-            columnGap="1.33%"
-          >
-            {filteredProducts.map((product) => (
-                    <Product product={product} key={product.id} />
-              ))}
-          </Box>
-        </Box>
-      );
-    };
+  const filteredProducts =
+    value === "all"
+      ? products
+      : value === "TOP"
+      ? Top
+      : value === "BOTTOM"
+      ? Bottom
+      : Dress;
+
+  const paginationCount = Math.ceil(filteredProducts.length / productsPerPage);
+
+  const handlePageChange = (event, value) => {
+    setCurrentPage(value);
+  };
+
+  return (
+    <Box width="80%" margin="80px auto">
+      <Typography variant="h3" textAlign="center">
+        Our Featured <b>Products</b>
+      </Typography>
+      <Tabs
+        textColor="primary"
+        indicatorColor="primary"
+        value={value}
+        onChange={handleChange}
+        centered
+        sx={{
+          m: "25px",
+          "& .MuiTabs-flexContainer": {
+            flexWrap: "wrap",
+          },
+        }}
+      >
+        <Tab label="ALL" value="all" />
+        <Tab label="TOP" value="TOP" />
+        <Tab label="BOTTOM" value="BOTTOM" />
+        <Tab label="DRESS" value="DRESS" />
+      </Tabs>
+      <Box
+        margin="0 auto"
+        display="grid"
+        gridTemplateColumns="repeat(auto-fill, 300px)"
+        justifyContent="space-around"
+        rowGap="20px"
+        columnGap="1.33%"
+      >
+        {filteredProducts.slice(startIndex, endIndex).map((product) => (
+          <Product product={product} key={product.id} />
+        ))}
+      </Box>
+      <Stack spacing={2} sx={{ justifyContent: "center" }}>
+        <Pagination
+          count={paginationCount}
+          page={currentPage}
+          onChange={handlePageChange}
+          variant="outlined"
+          shape="rounded"
+          sx={{ margin: "auto" }}
+        />
+      </Stack>
+    </Box>
+  );
+};
 
 export default Products;
